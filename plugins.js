6c49eb0ee1221a08d7380b451615e60d761ed49c
StudioOneScripts <StudioOneScripts.users.noreply.github.com>
2017-07-01 15:29:41 -0400

    updated

--- plugins.js
+++ plugins.js
@@ -2,22 +2,6 @@
  
 function insertPlugin(target) {
 
-    /*  This script allows loading any plugin directly to selected tracks via key command
-        or via macro toolbar buttons.  
-
-        It works by firing the Console | Add Insert pop-up list and executing X "Down"
-        commands to get to any plugin in the list and then loads it.  That list is fluid
-        and may change as you add new plugins and if so you'll have to edit the position
-        numbers below and recompile the script.
-
-        ** Always move the mouse up out of arrange before firing the script with a bound
-        hotkey.   If the mouse cursor is in the arrange space it will auto highlight the
-        plugin in that pop-up list that's on the same vertical level as the mouse cursor.
-        
-        Click the title bar or something before hitting the hot key, or use macro toolbar 
-        buttons to load your favorite plugins.2222
-    */
-
     var position; 
     this.interfaces = [Host.Interfaces.IEditTask];
     this.prepareEdit = function () { return Host.Results.kResultOk; }
